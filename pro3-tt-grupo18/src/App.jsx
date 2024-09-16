b48f344e3f34836c6d596261329a26a971826219
matias-inda <indam@udesa.edu.ar>
2024-09-16 17:47:37 -0300

    Home, falta css

--- pro3-tt-grupo18/src/App.jsx
+++ pro3-tt-grupo18/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
+import Populares from './components/Populares/Populares';
 
 function App() {
   let menu = [
@@ -11,9 +12,12 @@ function App() {
   ]
   return (
     <React.Fragment>
-      <Header elementosMenu={menu}/>
-      <h1>Películas</h1>
+      <Header elementosMenu = {menu}/>
+      <h1>Cuevana 2</h1>
       <main>
+      <section className="top-data">
+      <Populares/>
+      </section>
 
       </main>
       <Footer/>
---
+++ pro3-tt-grupo18/src/components/CardPopulares/CardPopulares.js
@@ -0,0 +1,35 @@
+import React, {Component} from "react";
+
+class CardPopulares extends Component {
+    constructor(props){
+        super(props)
+        this.state = ({
+            data: "",
+            varMas: false,
+        })
+    }
+    verMas(){
+        this.setState({
+            verMas: !this.state.verMas
+        })
+    }
+    render(){
+        const {title, overview, poster_path, } = this.props.results
+        return(
+            <>
+                <article className="peliculas-grid">
+                    <img src={`https://image.tmdb.org/t/p/w500/${poster_path}`} alt=""/>
+                    <h2>{title}</h2>
+                    <p className="more" onClick={() => this.verMas()}>{this.state.verMas ? "Ocultar descripción" : "Ver descripción" }</p>
+                    {this.state.verMas && (<section className="extra">
+                        <p>Descripción: {overview}</p>
+                    </section>)}
+                    <button>Ir a detalle</button>
+                    <button>Añadir a favoritos</button>
+                </article>
+            </>
+        )
+
+    }
+}
+export default CardPopulares;
