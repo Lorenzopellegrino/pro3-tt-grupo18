690dba65c02d015325d6cc9b599900856127abb2
matias-inda <indam@udesa.edu.ar>
2024-09-16 15:07:17 -0300

    Prueba, emprolijamiento App.js

--- pro3-tt-grupo18/src/index.js
+++ pro3-tt-grupo18/src/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function App() {
+  return (
+    <React.Fragment>
+      <Navbar elementosMenu={menu}/>
+      <h1>Películas</h1>
+      <main>
 
+      </main>
+      <Footer />
+    </React.Fragment>
+  );
+}
 
-reportWebVitals();
+export default App;
