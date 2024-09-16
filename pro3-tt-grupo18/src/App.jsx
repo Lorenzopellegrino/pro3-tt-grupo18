60a972df896be43d46e5c9684ebe632040536dfa
matias-inda <indam@udesa.edu.ar>
2024-09-16 16:27:38 -0300

    Header y footer

--- pro3-tt-grupo18/src/App.jsx
+++ pro3-tt-grupo18/src/App.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
-
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
 
 function App() {
+  let menu = [
+    "Home",
+    "Peliculas recientes",
+    "Películas Populares",
+    "Favoritos"
+  ]
   return (
     <React.Fragment>
+      <Header elementosMenu={menu}/>
       <h1>Películas</h1>
       <main>
 
       </main>
+      <Footer/>
     </React.Fragment>
   );
 }
