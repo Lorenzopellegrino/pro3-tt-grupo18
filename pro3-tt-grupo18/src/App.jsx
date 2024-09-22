43240b478fb5647b486a8a43376f01b148567345
Stefanocavallo <cavallos@udesa.edu.ar>
2024-09-22 15:53:19 -0300

    En curso pagina detalle

--- pro3-tt-grupo18/src/App.jsx
+++ pro3-tt-grupo18/src/App.jsx
@@ -3,6 +3,7 @@ import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import Populares from './components/Populares/Populares';
 import Upcoming from './components/Upcoming/Upcoming';
+import Detalle from './components/Detalle';
 
 function App() {
   let menu = [
---
+++ pro3-tt-grupo18/src/components/Detalle/Detalle.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import CardDetalle from '../CardDetalle/CardDetalle';
+
+class Detalle extends Component{
+    constructor(props){
+        super(props);
+        this.state={
+            peliculas: []
+        };
+    }
+}
+
+componentDidMount()
