6906546c2fe64660a6171d604392531d95a3a60b
matias-inda <indam@udesa.edu.ar>
2024-09-29 19:09:43 -0300

    No se envian los results ni a detalle ni a fav ni a vermas

--- pro3-tt-grupo18/src/components/CardPopulares/CardPopulares.js
+++ pro3-tt-grupo18/src/components/CardPopulares/CardPopulares.js
@@ -55,35 +55,41 @@ class CardPopulares extends Component {
             verMas: !this.state.verMas
         })
     }
-    render(){
-        const {title, overview, poster_path, } = this.props.results;
-        const {esFav} = this.state;
-        return(
+    render() {
+        const { results } = this.props;
+        const { esFav } = this.state;
+
+        if (!results) {
+            return <p>Loading...</p>;
+        }
+
+        const { title, overview, poster_path } = results;
+        return (
             <>
                 <article className="peliculas-grid">
-                    <img src={`https://image.tmdb.org/t/p/w500/${poster_path}`} alt="" className="fotopelicula"/>
+                    <img src={`https://image.tmdb.org/t/p/w500/${poster_path}`} alt="" className="fotopelicula" />
                     <h3>{title}</h3>
-                    <p className="more" onClick={() => this.verMas()}>{this.state.verMas ? "Ocultar descripción" : "Ver descripción" }</p>
-                    {this.state.verMas && (<section className="extra">
-                        <p>Descripción: {overview}</p>
-                    </section>)}
-                    <button><Link to= "/movie/:id"> Ir a detalle </Link></button>
+                    <p className="more" onClick={() => this.verMas()}>{this.state.verMas ? "Ocultar descripción" : "Ver descripción"}</p>
+                    {this.state.verMas && (
+                        <section className="extra">
+                            <p>Descripción: {overview}</p>
+                        </section>
+                    )}
+                    <button>Ir a detalle</button>
                     <button onClick={() => this.Favoritas()}>
                         {esFav ? (
                             <p>
                                 <FaHeart color="red" /> Eliminar favorita{" "}
                             </p>
-                        ):(
+                        ) : (
                             <p>
                                 <FaRegHeart /> Marcar favorita{" "}
                             </p>
-                        )
-                    }
+                        )}
                     </button>
                 </article>
             </>
-        )
-
+        );
     }
 }
 export default CardPopulares;
