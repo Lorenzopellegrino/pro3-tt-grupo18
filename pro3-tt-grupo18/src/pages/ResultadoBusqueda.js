4aa3b37ccbd1bad6bdfd4b0c3f6c417570edb98b
Lorenzopellegrino <lpellegrino@udesa.edu.ar>
2024-09-30 12:06:46 -0300

    resultado busqueda

--- pro3-tt-grupo18/src/pages/ResultadoBusqueda.js
+++ pro3-tt-grupo18/src/pages/ResultadoBusqueda.js
@@ -1,22 +1,24 @@
 import React, { Component } from "react";
 import CardPopulares from "../components/CardPopulares/CardPopulares";
-import Loader from "../components/Loader/Loader";
+import Loader from "../components/Loader/Loader"
 
 class ResultadoBusqueda extends Component {
     constructor(props) {
-        super(props);
+        super(props)
         this.state = {
             peliculas: [],
-            cargando: true,
+            cargando: true
         };
     }
 
     componentDidMount() {
-        this.setState({ cargando: true });
+        this.setState({
+            cargando: true
+        })
         fetch(`https://api.themoviedb.org/3/search/movie?query=${this.props.location.state.query}&api_key=bf0e25b4b648e8ee928c7dede4d12427`)
             .then((response) => response.json())
             .then((data) => this.setState({ peliculas: data.results, cargando: false }))
-            .catch((e) => console.log(e));
+            .catch((e) => console.log(e))
     }
 
     render() {
@@ -37,8 +39,8 @@ class ResultadoBusqueda extends Component {
                     )}
                 </div>
             </>
-        );
+        )
     }
 }
 
-export default ResultadoBusqueda;
+export default ResultadoBusqueda
