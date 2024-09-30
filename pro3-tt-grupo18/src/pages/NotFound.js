6b651c07fcb46c4edc23bec328fb51fa6041da2e
Lorenzopellegrino <lpellegrino@udesa.edu.ar>
2024-09-30 16:31:16 -0300

    not found correcion

--- pro3-tt-grupo18/src/pages/NotFound.js
+++ pro3-tt-grupo18/src/pages/NotFound.js
@@ -1,24 +1,26 @@
-import React, { Component } from 'react'
-import Header from '../components/Header/Header'
-import Footer from '../components/Footer/Footer'
+import React, { Component } from 'react';
 
 class NotFound extends Component {
-    constructor(props){
-        super(props)
-        this.state = {}
+    constructor(props) {
+        super(props);
+        this.state = {};
     }
 
-
-    render(){
-        return(
+    render() {
+        return (
             <>
-            <div>
-                Not Found
-            </div>
-            
+                <div className="not-found-container">
+                    <h1>404 - Not Found</h1>
+                    <img 
+                        src="https://media1.tenor.com/m/lx2WSGRk8bcAAAAC/pulp-fiction-john-travolta.gif" 
+                        alt="Not Found" 
+                    />
+                    <p>Lo sentimos, la página que buscas no existe.</p>
+                    <a href="/">Regresar a la página principal</a>
+                </div>
             </>
-        )
+        );
     }
 }
 
-export default NotFound
+export default NotFound;
